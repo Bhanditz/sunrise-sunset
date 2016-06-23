3ac994e81d4e39068ffb9ff22d141f778a951c76
trainorpj <trainorpj@gmail.com>
2016-06-23 12:21:43 -0400

    Begin styling the chart.  Just colors for now.

--- js/chart.js
+++ js/chart.js
@@ -1,44 +1,56 @@
 var sunChart = {
 
-  drawChart: function(){
+    drawChart: function() {
 
-    var maxDayLength = 60 * 24 - 1;
+        //Dimensions of the main svg
+        var widthSvg = 800,
+            heightSvg = 900;
 
-    var svgWidth = 800,
-        svgHeight = 900;
+        //Height of each rectangle
+        var heightDay = 10;
 
-    var scaleBoxWidth = d3.scale.linear()
-                            .domain([0, maxDayLength])
-                            .range([0, svgWidth])
-                            ;
+        //Scaling section
+        var maxDayLength = 60 * 24 - 1;
+        var scaleBoxWidth = d3.scale.linear()
+            .domain([0, maxDayLength])
+            .range([0, widthSvg]);
 
-    var vis = d3.select("#vis");
+        var vis = d3.select("#vis");
 
-    var canvas = vis.append("svg")
-            .attr("width", svgWidth)
-            .attr("height", svgHeight)
-            ;
+        var canvas = vis.append("svg")
+            .attr("width", widthSvg)
+            .attr("height", heightSvg);
 
-    d3.json("test/data.json", function(error,dataset){
+        canvas.append("rect")
+            .attr({
+                x: 0,
+                y: 0,
+                width: widthSvg,
+                height: heightSvg,
+                id: "rectBackground"
+            });
 
-      console.log(dataset);
+        //Make the chart
+        d3.json("test/data.json", function(error, dataset) {
 
-      var dataRect = canvas.selectAll("rects")
-                      .data(dataset)
-                      .enter()
-                      ;
 
-      var heightDay = 10;
+            var dataRect = canvas.selectAll("rects")
+                .data(dataset)
+                .enter();
 
-      dataRect.append("rect")
-        .attr("x", function(d){return scaleBoxWidth(d.rise);})
-        .attr("y", function(d,i){return i*heightDay;})
-        .attr("height", heightDay)
-        .attr("width", function(d){return scaleBoxWidth(d.set)})
-        .style("fill","red")
-        .style("stroke", "black")
-        ;
-    });
+            dataRect.append("rect")
+                .attr("x", function(d) {
+                    return scaleBoxWidth(d.rise);
+                })
+                .attr("y", function(d, i) {
+                    return i * heightDay;
+                })
+                .attr("height", heightDay)
+                .attr("width", function(d) {
+                    return scaleBoxWidth(d.set)
+                })
+                .classed("rectDay", true);
+        });
 
-  }
+    }
 };
