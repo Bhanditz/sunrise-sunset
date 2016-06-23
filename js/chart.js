59003c03d319456e2f1b0a89f7eaf6efeab5fbe6
trainorpj <trainorpj@gmail.com>
2016-06-23 11:55:52 -0400

    d3 functionality working with test data

--- js/chart.js
+++ js/chart.js
@@ -1,12 +1,44 @@
-sunChart = {
+var sunChart = {
 
   drawChart: function(){
 
-    vis = d3.select("#vis");
+    var maxDayLength = 60 * 24 - 1;
 
-    canvas = vis.append("svg")
-            .attr("width",800)
-            .attr("height",900)
+    var svgWidth = 800,
+        svgHeight = 900;
+
+    var scaleBoxWidth = d3.scale.linear()
+                            .domain([0, maxDayLength])
+                            .range([0, svgWidth])
+                            ;
+
+    var vis = d3.select("#vis");
+
+    var canvas = vis.append("svg")
+            .attr("width", svgWidth)
+            .attr("height", svgHeight)
             ;
+
+    d3.json("test/data.json", function(error,dataset){
+
+      console.log(dataset);
+
+      var dataRect = canvas.selectAll("rects")
+                      .data(dataset)
+                      .enter()
+                      ;
+
+      var heightDay = 10;
+
+      dataRect.append("rect")
+        .attr("x", function(d){return scaleBoxWidth(d.rise);})
+        .attr("y", function(d,i){return i*heightDay;})
+        .attr("height", heightDay)
+        .attr("width", function(d){return scaleBoxWidth(d.set)})
+        .style("fill","red")
+        .style("stroke", "black")
+        ;
+    });
+
   }
-}
+};
