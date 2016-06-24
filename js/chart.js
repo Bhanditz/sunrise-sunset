14a4bd4ce6a9a1590254edbf2365752df2ca7ea4
trainorpj <trainorpj@gmail.com>
2016-06-24 11:24:03 -0400

    Styling and calculation fixes

--- js/chart.js
+++ js/chart.js
@@ -8,6 +8,7 @@ var sunChart = {
 
         //Height of each rectangle
         var heightDay = 10;
+        var bufferDay = 1;
 
         //Scaling section
         var maxDayLength = 60 * 24 - 1;
@@ -21,32 +22,43 @@ var sunChart = {
             .attr("width", widthSvg)
             .attr("height", heightSvg);
 
-        canvas.append("rect")
+  /*      canvas.append("rect")
             .attr({
                 x: 0,
                 y: 0,
                 width: widthSvg,
                 height: heightSvg,
-                id: "rectBackground"
+                id: "rect-background"
             });
-
+*/
         //Make the chart
         d3.json("test/data.json", function(error, dataset) {
 
             var dataRect = canvas.selectAll("rects")
                 .data(dataset)
-                .enter();
+                .enter()
+                .append('g')
+                .classed("holder-rect", true);
+
+            dataRect.append("rect")
+                .attr("y", function(d, i){return i * (heightDay + bufferDay);})
+                .attr({
+                    x: 0,
+                    width: widthSvg,
+                    height: heightDay,
+                    class: "rect-background"
+                });
 
             dataRect.append("rect")
                 .attr("x", function(d) {
                     return scaleBoxWidth(d.rise);
                 })
                 .attr("y", function(d, i) {
-                    return i * heightDay;
+                    return i * (heightDay + bufferDay);
                 })
                 .attr("height", heightDay)
                 .attr("width", function(d) {
-                    return scaleBoxWidth(maxDayLength - d.set);
+                    return scaleBoxWidth(d.set - d.rise);
                 })
                 .classed("rectDay", true);
         });
