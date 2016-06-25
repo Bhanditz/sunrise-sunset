02c8694a146f83a0d38ed9f6a830b690da3a2468
trainorpj <trainorpj@gmail.com>
2016-06-25 10:08:50 -0400

    Web scraper works; converts text on page to string

--- js/page-request.js
+++ js/page-request.js
@@ -3,4 +3,29 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 
-var app = express();
+
+pageLoad = {
+
+    loadString: function(state, place) {
+        var url = "http://aa.usno.navy.mil/cgi-bin/aa_rstablew.pl?ID=AA&year=2016&task=0&state=" + state + " &place=" + place;
+
+        request(url, function(error, response, body) {
+            console.log("enter");
+
+            if (!error) {
+                var $ = cheerio.load(body);
+
+                var data = $("pre").html();
+
+                console.log(data);
+                console.log(state, place);
+
+            } else {
+                console.log("ERROR: " + error);
+            }
+        });
+
+    }
+};
+
+pageLoad.loadString("FL","miami");
